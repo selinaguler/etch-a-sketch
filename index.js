d274e805172bb1177a400151a680251eb7528de4
selinaguler <selin.guler@tprs.stud.vu.lt>
2025-10-07 12:54:14 +0300

    commit

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 function makeRows(rows, cols) {
   const box = document.getElementById("box");
+
+  box.innerHTML = ""; // this will clear old divs before we set new columns and rows.
   // set variable value on box
   box.style.setProperty('--grid-rows', rows);
   box.style.setProperty('--grid-cols', cols);
@@ -9,22 +11,32 @@ function makeRows(rows, cols) {
     cell.className = "grid-item";
     cell.innerText = (c + 1);
     box.appendChild(cell);
+
+    //cell.addEventListener("mouseenter", () => {
+    //  cell.style.backgroundColor = "red";
+    //});
   };
 };
 
 makeRows(16, 16);
 
-const element = document.querySelectorAll(".grid-item");
 
-element.addEventListener('mouseenter', () => {
-  element.style.backgroundColor = 'red';
-});
+
+
+
 
 const button = document.getElementById("button");
 
 button.addEventListener("click", () => {
-  const answer = Number(prompt("New grid number:"));
-  if (!isNaN(answer) && answer > 0) {
-    makeRows(answer, answer);
+  const rows = Number(prompt("Enter number of rows (max 50):"));
+  const cols = Number(prompt("Enter number of columns (max 50):"));
+
+  if (
+    !isNaN(rows) && rows > 0 && rows <= 65 &&
+    !isNaN(cols) && cols > 0 && cols <= 65
+  ) {
+    makeRows(rows, cols);
+  } else {
+    alert("Please enter positive numbers up to 50!");
   }
 });
